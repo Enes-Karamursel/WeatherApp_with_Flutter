bb74c958237b9fa3b59308cc2a4ea785e38ff165
Enes <eneskaramursel@gmail.com>
2024-07-31 16:35:51 +0300

    cards are updated. Also, some UI changes.

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ void main() {
   runApp(
     ChangeNotifierProvider(
       create: (context) => CityWeatherProvider(),
-      child: MaterialApp(
+      child: const MaterialApp(
         home: Home(),
       ),
     ),
@@ -21,40 +21,49 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(
+        title: const Text(
           'Weather App',
           style: TextStyle(
             color: Colors.white,
+            fontFamily: 'YourFontFamily',
             fontSize: 24,
             fontWeight: FontWeight.bold,
           ),
         ),
         centerTitle: true,
-        backgroundColor: Colors.pinkAccent,
+        backgroundColor: const Color(0xFF093DAB),
         actions: [
           Consumer<CityWeatherProvider>(
             builder: (context, provider, child) {
-              return DropdownButton<String>(
-                value: provider.temperatureUnit.isNotEmpty ? provider.temperatureUnit : null,
-                hint: Text(
-                  'Select Unit ',
-                  style: TextStyle(color: Colors.white, fontSize: 15)
+              return Theme(
+                data: Theme.of(context).copyWith(
+                  canvasColor: Colors.black.withOpacity(0.7), // Açılan kutunun arka plan rengi
                 ),
-                icon: Icon(Icons.sunny_snowing, color: Colors.white, size: 20),
-                underline: Container(
-                  height: 1.5,
-                  color: Colors.white,
+                child: DropdownButton<String>(
+                  value: provider.temperatureUnit.isNotEmpty ? provider.temperatureUnit : null,
+                  hint: const Text(
+                    'Select Unit ',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  icon: const Icon(Icons.sunny_snowing, color: Colors.white),
+                  underline: Container(
+                    height: 2,
+                    color: Colors.white,
+                  ),
+                  onChanged: (String? newValue) {
+                    provider.setTemperatureUnit(newValue ?? 'Celsius');
+                  },
+                  items: <String>['Celsius', 'Fahrenheit']
+                      .map<DropdownMenuItem<String>>((String value) {
+                    return DropdownMenuItem<String>(
+                      value: value,
+                      child: Text(
+                        value,
+                        style: const TextStyle(color: Colors.white), // Yazı rengini beyaz yap
+                      ),
+                    );
+                  }).toList(),
                 ),
-                onChanged: (String? newValue) {
-                  provider.setTemperatureUnit(newValue ?? 'Celsius');
-                },
-                items: <String>['Celsius', 'Fahrenheit']
-                    .map<DropdownMenuItem<String>>((String value) {
-                  return DropdownMenuItem<String>(
-                    value: value,
-                    child: Text(value),
-                  );
-                }).toList(),
               );
             },
           ),
@@ -75,35 +84,12 @@ class Home extends StatelessWidget {
                     ),
                   );
                 },
-                child: Stack(
-                  children: [
-                    Card(
-                      margin: EdgeInsets.all(10.0),
-                      child: ListTile(
-                        title: Text(city.name),
-                        trailing: Text(
-                          provider.temperatureUnit == 'Celsius'
-                              ? '${city.temperature}°C'
-                              : '${city.toFahrenheit()}°F',
-                        ),
-                      ),
-                    ),
-                    Positioned(
-                      top: 1,
-                      right: 1,
-                      child: CircleAvatar(
-                        radius: 10,
-                        backgroundColor: Colors.black.withOpacity(0.1),
-                        child: IconButton(
-                          padding: EdgeInsets.zero,
-                          icon: Icon(Icons.close, color: Colors.black, size: 14),
-                          onPressed: () {
-                            provider.removeCity(index);
-                          },
-                        ),
-                      ),
-                    ),
-                  ],
+                child: WeatherCard(
+                  city: city,
+                  temperatureUnit: provider.temperatureUnit,
+                  onDelete: () {
+                    provider.removeCity(index);
+                  },
                 ),
               );
             },
@@ -119,8 +105,8 @@ class Home extends StatelessWidget {
             },
           );
         },
-        child: Icon(Icons.add, color: Colors.white),
-        backgroundColor: Colors.pinkAccent,
+        backgroundColor: const Color(0xFF093DAB),
+        child: const Icon(Icons.add, color: Colors.white),
       ),
     );
   }
@@ -138,7 +124,7 @@ class _AddCityDialogState extends State<AddCityDialog> {
   @override
   Widget build(BuildContext context) {
     return AlertDialog(
-      title: Text('Add City'),
+      title: const Text('Add City'),
       content: Column(
         mainAxisSize: MainAxisSize.min,
         children: [
@@ -156,8 +142,7 @@ class _AddCityDialogState extends State<AddCityDialog> {
           onPressed: () async {
             final cityName = _controller.text;
             if (cityName.isNotEmpty) {
-              final provider =
-              Provider.of<CityWeatherProvider>(context, listen: false);
+              final provider = Provider.of<CityWeatherProvider>(context, listen: false);
               final isValid = await provider.addCity(cityName);
               if (isValid) {
                 Navigator.of(context).pop();
@@ -168,7 +153,7 @@ class _AddCityDialogState extends State<AddCityDialog> {
               }
             }
           },
-          child: Text('Add'),
+          child: const Text('Add'),
         ),
       ],
     );
@@ -179,8 +164,9 @@ class City {
   final String name;
   final int temperature;
   final String weatherDescription;
+  final String time;
 
-  City(this.name, this.temperature, this.weatherDescription);
+  City(this.name, this.temperature, this.weatherDescription, this.time);
 
   int toFahrenheit() {
     return ((temperature * 9 / 5) + 32).round();
@@ -189,7 +175,7 @@ class City {
 
 class CityWeatherProvider with ChangeNotifier {
   List<City> _cities = [];
-  final String _apiKey = '8b222abc8d47cc21c73e5e055b1936a9'; // OpenWeatherMap API anahtarınızı buraya ekleyin.
+  final String _apiKey = '8b222abc8d47cc21c73e5e055b1936a9';
 
   List<City> get cities => _cities;
   String _temperatureUnit = '';
@@ -204,7 +190,7 @@ class CityWeatherProvider with ChangeNotifier {
   Future<bool> addCity(String cityName) async {
     final cityData = await _fetchCityWeather(cityName);
     if (cityData != null) {
-      final city = City(cityName, cityData['temperature'], cityData['description']);
+      final city = City(cityName, cityData['temperature'], cityData['description'], cityData['time']);
       _cities.add(city);
       notifyListeners();
       return true;
@@ -216,15 +202,15 @@ class CityWeatherProvider with ChangeNotifier {
     final weatherUrl = 'https://api.openweathermap.org/data/2.5/weather?q=$cityName&units=metric&appid=$_apiKey';
     final response = await http.get(Uri.parse(weatherUrl));
     if (response.statusCode != 200) {
-      print('Failed to fetch weather for $cityName: ${response.body}');
       return null;
     }
 
     final weatherData = json.decode(response.body);
     final temperature = weatherData['main']['temp'].toInt();
     final description = weatherData['weather'][0]['description'];
+    final time = DateTime.now().toString();
 
-    return {'temperature': temperature, 'description': description};
+    return {'temperature': temperature, 'description': description, 'time': time};
   }
 
   void removeCity(int index) {
@@ -234,6 +220,93 @@ class CityWeatherProvider with ChangeNotifier {
     }
   }
 }
+
+class WeatherCard extends StatelessWidget {
+  final City city;
+  final String temperatureUnit;
+  final VoidCallback onDelete;
+
+  const WeatherCard({
+    super.key,
+    required this.city,
+    required this.temperatureUnit,
+    required this.onDelete
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: const EdgeInsets.all(10.0),
+      child: Container(
+        padding: const EdgeInsets.all(10.0),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          gradient: const LinearGradient(
+            colors: [Colors.blue, Colors.lightBlueAccent],
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+          ),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  city.name,
+                  style: const TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                ),
+                CircleAvatar(
+                  radius: 10,
+                  backgroundColor: Colors.black.withOpacity(0.5),
+                  child: IconButton(
+                    padding: EdgeInsets.zero,
+                    icon: const Icon(Icons.close, color: Colors.white, size: 16),
+                    onPressed: onDelete,
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 5),
+            Text(
+              temperatureUnit == 'Celsius'
+                  ? '${city.temperature}°C'
+                  : '${city.toFahrenheit()}°F',
+              style: const TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+            ),
+            const SizedBox(height: 5),
+            Text(
+              city.weatherDescription,
+              style: const TextStyle(
+                fontSize: 18,
+                color: Colors.white,
+              ),
+            ),
+            const SizedBox(height: 5),
+            Text(
+              city.time,
+              style: const TextStyle(
+                fontSize: 16,
+                color: Colors.white,
+              ),
+            ),
+            const SizedBox(height: 5),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
 
 class WeatherDetailScreen extends StatelessWidget {
   final City city;
@@ -247,7 +320,7 @@ class WeatherDetailScreen extends StatelessWidget {
     switch (city.weatherDescription.toLowerCase()) {
       case 'clear sky':
       case 'few clouds':
-        backgroundImage = 'assets/sunny.png';
+        backgroundImage = 'assets/sunny2.png';
         break;
       case 'rain':
       case 'shower rain':
@@ -261,14 +334,21 @@ class WeatherDetailScreen extends StatelessWidget {
       case 'fog':
         backgroundImage = 'assets/foggy.png';
         break;
+      case 'scattered clouds':
+        backgroundImage = 'assets/cloudy2.png';
       default:
         backgroundImage = 'assets/cloudy.png';
     }
 
     return Scaffold(
       appBar: AppBar(
-        title: Text('${city.name} Weather'),
-        backgroundColor: Colors.pinkAccent,
+        title: Text(
+          '${city.name} Weather',
+          style: const TextStyle(
+            color: Colors.white,
+          ),
+        ),
+        backgroundColor: const Color(0xFF093DAB),
       ),
       body: Container(
         decoration: BoxDecoration(
@@ -280,15 +360,76 @@ class WeatherDetailScreen extends StatelessWidget {
         child: Center(
           child: Consumer<CityWeatherProvider>(
             builder: (context, provider, child) {
-              return Text(
-                '${city.name}\n${provider.temperatureUnit == 'Celsius' ? city.temperature : city.toFahrenheit()}°${provider.temperatureUnit == 'Celsius' ? 'C' : 'F'}\n${city.weatherDescription}',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: 32,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white,
-                  backgroundColor: Colors.black.withOpacity(0.5),
-                ),
+              return Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  // Derece
+                  Text(
+                    '${provider.temperatureUnit == 'Celsius' ? city.temperature : city.toFahrenheit()}°${provider.temperatureUnit == 'Celsius' ? 'C' : 'F'}',
+                    style: const TextStyle(
+                      fontSize: 64,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                      shadows: [
+                        Shadow(
+                          offset: Offset(2.0, 2.0),
+                          blurRadius: 5.0,
+                          color: Colors.black,
+                        ),
+                      ],
+                    ),
+                  ),
+                  // Hava durumu açıklaması
+                  Text(
+                    city.weatherDescription,
+                    style: const TextStyle(
+                      fontSize: 28,
+                      fontWeight: FontWeight.w400,
+                      color: Colors.white,
+                      letterSpacing: 1.2,
+                      shadows: [
+                        Shadow(
+                          offset: Offset(2.0, 2.0),
+                          blurRadius: 2.0,
+                          color: Colors.black,
+                        ),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(height: 140),
+                  // Şehir ismi
+                  const Text('My Location',
+                    style: TextStyle(
+                      fontSize: 30,
+                      fontWeight: FontWeight.w600,
+                      color: Colors.white,
+                      letterSpacing: 1.1,
+                      shadows: [
+                        Shadow(
+                          offset: Offset(2.0, 2.0),
+                          blurRadius: 2.0,
+                          color: Colors.black,
+                        ),
+                      ],
+                    ),
+                  ),
+                  Text(
+                    city.name,
+                    style: const TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.w600,
+                      color: Colors.white,
+                      letterSpacing: 1.1,
+                      shadows: [
+                        Shadow(
+                          offset: Offset(2.0, 2.0),
+                          blurRadius: 2.0,
+                          color: Colors.black,
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
               );
             },
           ),
